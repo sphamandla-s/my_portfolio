b87f76779ef61a73719c0f5603622f19c7d8e768
smdletsh021 <smdletsh021@student.wethinkcode.co.za>
2022-12-21 13:15:21 +0200

    near done with about section

---
+++ app.js
@@ -0,0 +1,52 @@
+const tabLinks = document.querySelectorAll('.tab-link');
+const skills = document.querySelector('#skill-tab');
+const education = document.querySelector('#education-tab');
+const experience = document.querySelector('#experience-tab');
+
+
+skills.addEventListener('click', () => {
+    //skills.classList.remove('active-link')
+    openTab('skill')
+})
+
+education.addEventListener('click', () => {
+    openTab('education')
+})
+
+experience.addEventListener('click', () => {
+    openTab('experience')
+})
+
+
+function openTab(clicked) {
+    switch (clicked) {
+        case 'skill':
+            skills.classList.add('active-link');
+            education.classList.remove('active-link');
+            experience.classList.remove('active-link');
+            document.querySelector('#skills').classList.add('active-tab');
+            document.querySelector('#education').classList.remove('active-tab');
+            document.querySelector('#experience').classList.remove('active-tab');
+            break;
+        case 'education':
+            education.classList.add('active-link');
+            skills.classList.remove('active-link');
+            experience.classList.remove('active-link');
+            document.querySelector('#skills').classList.remove('active-tab');
+            document.querySelector('#education').classList.add('active-tab');
+            document.querySelector('#experience').classList.remove('active-tab');
+            break;
+
+        case 'experience':
+            experience.classList.add('active-link');
+            education.classList.remove('active-link');
+            skills.classList.remove('active-link');
+            document.querySelector('#skills').classList.remove('active-tab');
+            document.querySelector('#education').classList.remove('active-tab');
+            document.querySelector('#experience').classList.add('active-tab');
+            break;
+
+        default:
+            break;
+    }
+}
